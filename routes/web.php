c8578bf7da9824494c03ed7805d6fac10ed3399b
manuelcamaioni <camaioni.manuel@gmail.com>
2023-07-18 13:21:46 +0200

    passed data through web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $string = 'HelloWorld';
+
+    return view('home', compact('string'));
 });
